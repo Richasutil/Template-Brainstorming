016ac2749b79946826dc94cad82d7126b1f32770
Richasutil <162706438+Richasutil@users.noreply.github.com>
2024-06-14 10:36:43 -0300

    commit

---
+++ script.js
@@ -0,0 +1,22 @@
+const caixaPrincipal = document.querySelectorAll(".caixa-principal");
+const caixaPerguntas = document.querySelectorAll(".caixa-perguntas");
+const caixaAlternativas = document.querySelectorAll(".caixa-alternativas");
+const caixaResultado = document.querySelectorAll(".caixa-resultado");
+const textoResultado = document.querySelectorAll(".texto-resultado");
+
+const perguntas = [
+    {
+        enunciado: Pergunta 1,
+        alternativas: [
+            "Alternativa 1,
+         Alternativa 2"
+        ]
+    },
+    {
+        enunciado: Pergunta 1,
+        alternativas: [
+            "Alternativa 1,
+             Alternativa 2"
+        ]
+    }
+];
